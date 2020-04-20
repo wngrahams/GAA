518d211117d7dbd8e94a12c5fbbde98d9cf23c14
wngrahams <w.grahamstubbs@gmail.com>
2020-04-20 12:50:33 +0200

    moved parameters and utility functions to their own files

--- src/sw/ga-params.h
+++ src/sw/ga-params.h
@@ -8,9 +8,11 @@
 #ifndef _GA_PARAMS_H_
 #define _GA_PARAMS_H_
 
-#define NUM_OF_GENERATIONS 10
 #define CROSSOVER_PROB 0.7
 #define MUTATION_PROB 0.001
+
+#define NUM_OF_GENERATIONS 10
+#define POP_SIZE 100
 
 #endif /* _GA_PARAMS_H_ */
 
