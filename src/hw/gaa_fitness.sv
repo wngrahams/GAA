9dd892ec2ed7a69e23868e5a728ae3a68fbc8ee3
Graham Stubbs <wgs2113@columbia.edu>
2020-05-12 10:19:09 -0400

    sdram mmap works here - avalon mm master from hps to sdram controller

--- src/hw/gaa_fitness.sv
+++ src/hw/gaa_fitness.sv
@@ -1,15 +1,91 @@
+
 module gaa_fitness(input logic clk,
-		   input logic reset,
+		   input  logic       reset,
 
-		   input logic[1:0] address,
-		   input logic chipselect,
-		   input logic write,
-		   input logic[7:0] writedata,
-	           input logic read,
-		   output logic[7:0] readdata);
+		   /* Avalon MM slave signals */
+		   input  logic[ 1:0] hps_address,
+		   input  logic       hps_chipselect,
+		   input  logic       hps_write,
+		   input  logic[ 7:0] hps_writedata,
+	           input  logic       hps_read,
+		   output logic[ 7:0] hps_readdata,
+		   output logic       hps_waitrequest,
+                   /* End Avalon MM slave signals */
 
-	logic [7:0] p1, p2, p1xorp2;
+		   /* Avalon MM master signals */
+                   output logic[24:0] sdram_address,
+                   output logic[ 1:0] sdram_byteenable_n,
+	           output logic       sdram_chipselect,
+		   output logic       sdram_read_n,
+                   input  logic[15:0] sdram_readdata,
+		   input  logic       sdram_readdatavalid,
+                   input  logic       sdram_waitrequest
+                   /* End Avalon MM master signals */
+);
 
+	//logic [7:0] p1, p2, p1xorp2;
+	logic [24:0] read_addr;
+	enum logic [2:0] {READY, WAIT, DONE} read_state;
+
+	// this should be unnecessary, just use always comb once you have proper addresses and data widths
+	always_ff @(posedge clk) begin
+		if (hps_chipselect && hps_read) begin
+			case (hps_address)
+				2'b00:   read_addr <= 25'd0;
+				2'b01:   read_addr <= 25'd2;
+				2'b10:   read_addr <= 25'd4;
+				default: read_addr <= 25'd0;
+			endcase
+		end
+	end
+
+	always_ff @(posedge clk) begin
+		case (read_state)
+			/*
+			READY: begin
+				
+			end
+
+			START_READ: begin
+				sdram_read_n <= 1'b0;
+				read_state <= WAIT;
+			end
+			*/
+
+			READY: begin
+				if (hps_chipselect && hps_read) begin
+					sdram_chipselect <= 1'b1;
+					read_state <= WAIT;
+				end
+				else read_state <= READY;
+			end
+
+			WAIT: begin
+				if (sdram_readdatavalid && !sdram_waitrequest) read_state <= DONE;
+				else read_state <= WAIT;
+			end
+
+			DONE: begin
+				hps_readdata <= sdram_readdata[7:0];
+				read_state <= WAIT;
+			end
+			default: read_state <= READY;
+		endcase
+	end
+
+	always_comb begin
+                //p1xorp2 = p1 ^ p2;
+                //hps_waitrequest = sdram_waitrequest;
+		//sdram_address = read_addr;
+		//sdram_read_n = ~hps_read;
+		if (reset) hps_waitrequest = 1'b1;              // assert waitrequest during reset to avoid system lockup
+		else       hps_waitrequest = sdram_waitrequest;
+
+		sdram_address = read_addr;
+		sdram_read_n = ~hps_read;	
+        end
+
+	/*
 	always_ff @(posedge clk) begin
 		if (reset) begin
 			p1      <= 8'h00;
@@ -31,10 +107,6 @@ module gaa_fitness(input logic clk,
 			endcase
 		end
 	end
-
-	always_comb begin
-		p1xorp2 = p1 ^ p2;
-	end
-			
+	*/		
 endmodule
 
